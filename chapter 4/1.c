f6edce22403ffe08e4f3f1434f00ba56632d458a
fssudipta <154584022+fssudipta@users.noreply.github.com>
2024-03-25 03:46:44 +0600

    Add files via upload

--- chapter 4/1.c
+++ chapter 4/1.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main()
+{
+    int n, fact=1;
+    scanf("%d", &n);
+    for (int i=n; i>=1; i--)
+    {
+        fact=fact*i;
+    }
+    printf("%d", fact);
+    return 0;
+    //same style for other data types
+}
