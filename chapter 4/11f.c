20695de7b5ad82337ce94ffeaa814a4ca880a217
tamal <51084465+tasnim12@users.noreply.github.com>
2024-03-27 16:54:50 +0600

    commit

--- chapter 4/11f.c
+++ chapter 4/11f.c
@@ -6,7 +6,11 @@ int main()
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=i;j++)
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++)
         {
             printf("*");
         }
@@ -18,7 +22,7 @@ int main()
         {
             printf(" ");
         }
-        for(int j=1;j<=n-i;j++)
+        for(int j=1;j<=2*(n-i)-1;j++)
         {
             printf("*");
         }
